5d14c3112ba27a9c80cb76df26f548a9fb3ab679
Brice AIELLO <brice.aiello.fr@gmail.com>
2025-03-07 14:22:29 +0100

    Add create event endpoint

--- src/server.js
+++ src/server.js
@@ -1,9 +1,30 @@
 const express = require('express');
-const app = express();
+const { PrismaClient } = require('@prisma/client');
+
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Server is running');
+const app = express();
+const prisma = new PrismaClient()
+
+app.use(express.json()) // for parsing application/json
+
+// Create event endpoint
+app.post('/events', async (req, res) => {
+  const { title, description, date } = req.body;
+
+  try {
+    const event = await prisma.event.create({
+      data: {
+        title,
+        description,
+        date: new Date(date),
+      },
+    });
+
+    return res.status(201).json(event);
+  } catch (error) {
+    return res.status(500).json({ error: "Boom!"});
+  }
 })
 
 app.listen(port, () => {
