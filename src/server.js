56089f83b9a17f0a156afe1bc7eaed0bfc663e1b
Brice AIELLO <brice.aiello.fr@gmail.com>
2025-03-26 12:16:49 +0100

    Add small security layers (helmet + rate limiter)

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { rateLimit } = require('express-rate-limit');
+const helmet = require('helmet');
 const { z } = require('zod');
 
 const eventsController = require('./controllers/eventsController.js');
@@ -13,9 +15,17 @@ if (!process.env.JWT_SECRET) {
 
 const app = express();
 
+const limiter = rateLimit({
+	windowMs: 1 * 60 * 1000, // 1 minute
+	limit: 10, // Limit each IP to 100 requests per `window`
+	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+})
 
 // Global middlewares
+app.use(limiter);
 app.use(customLogMiddleware);
+app.use(helmet());
 app.use(express.json()) // for parsing application/json
 
 
